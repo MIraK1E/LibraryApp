1ed621cde0c5a9dee5f69ec992fdf8a59d871cbb
MIraK1E <mike.pattana@gmail.com>
2017-12-07 00:57:04 +0700

    create config file and declare parameter for Database

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Define Database Parameters
+        define("DB_HOST", "localhost");
+        define("DB_USER", "root");
+        define("DB_PASS", "");
+        define("DB_NAME", "LibraryApp");
+
+    //Define URL
+        define("ROOT_PATH", "/code/LibraryApp/");
+        define("ROOT_URL", "https://localhost/code/LibraryApp");
+?>
